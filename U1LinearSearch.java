9ac253ada715d1c2bb90e0c7197e8468c95b35de
Nirmit-Bhanushali <102681894+Nirmit-Bhanushali@users.noreply.github.com>
2026-03-05 22:25:12 +0530

    add Linear Search implementation in Java

---
+++ U1LinearSearch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int key;
+        boolean found = false;
+
+        System.out.print("Enter element to search: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element found at position " + (i + 1));
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
